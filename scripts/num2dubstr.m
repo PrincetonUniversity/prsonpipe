41bf62af065815f42eacea135083821444d74c61
mweaverdyck <miriamw@princeton.edu>
2017-04-20 18:59:06 -0400

    Set up new file structure, changed paths in preprocessesing scripts, changed names of scripts, updated funcs get_subs and added funcs whatisit

--- scripts/num2dubstr.m
+++
@@ -1,7 +0,0 @@
-function s = num2dubstr(n)
-if n > 9
-        s = num2str(n);
-    else
-        s = ['0' num2str(n)];
-end
-end
--- scripts/num2tristr.m
+++
@@ -1,9 +0,0 @@
-function s = num2tristr(n)
-    if n > 99
-        s = num2str(n);
-    elseif n > 9
-        s = ['0' num2str(n)];
-    else
-        s = ['00' num2str(n)];
-    end
-end
