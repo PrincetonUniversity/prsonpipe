b0c7247f508677ad26030ff31ac7fe6e2ac91b87
jmildner <jmildner@princeton.edu>
2017-09-27 17:10:07 -0400

    sub_level glm: update help, input, con list output

--- scripts/sub_level/glm/matlab_glm_SPM12W.m
+++ scripts/sub_level/glm/matlab_glm_SPM12W.m
@@ -18,3 +18,12 @@ spm12w_glm_compute('sid', sub, 'glm_file', pfile)
 spm12w_glm_contrast('sid', sub, 'glm_file', pfile)
 cd([SCRIPT_DIR filesep 'sub_level' filesep 'glm'])
 matlab_glm_masking(sub,pfile);
+
+% create text file with contrast names and numbers
+glm_dir = fullfile(ANALYSIS_DIR, glm.username, 'glm', glm.glm_name, sub);
+load(fullfile(glm_dir, 'SPM.mat'))
+con_table = struct2table(SPM.xCon);
+con_info = cell2mat({SPM.xCon.Vcon});
+con_table.con_file = {con_info.fname}';
+writetable(con_table(:, {'con_file', 'name'}), fullfile(glm_dir, 'contrast_list.txt'),...
+    'Delimiter', ' ')
